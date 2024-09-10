64001aaceda55d19959219918911889892a15f4a
Yaman Ayush <yamanayush27204@gmail.com>
2024-09-10 14:03:58 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
-// import { APIKEY, APPID } from '../keys'; // Adjust the path as needed
+import { APIKEY, APPID } from "./keys/apikey"; // Adjust the path as needed
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-   apiKey: APIKEY,
+   apiKey: <APIKEY />,
+   //apiKey: "AIzaSyB-Fg65OiUgUiZeMlpQ1hgLSxjnXW6vJYs",
   authDomain: "board-infinit-todo.firebaseapp.com",
   projectId: "board-infinit-todo",
   storageBucket: "board-infinit-todo.appspot.com",
   messagingSenderId: "560550811396",
-   appId: APPID,
+   //appId: "1:560550811396:web:7c84848198298cfa40e251",
+   appID: <APPID />,
 };
 
 // Initialize Firebase
